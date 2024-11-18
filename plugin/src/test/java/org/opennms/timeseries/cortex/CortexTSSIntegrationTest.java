6ca976cd280c8a5682059aea0bae47d20177b27a
Ronny Trommer <ronny@no42.org>
2024-11-18 12:50:56 +0100

    feat: Upgrade Cortex from 1.8.0 to 1.18.1
    Create a compose stack using Cortex 1.18.1. We have to enable out-of-order support to get the
    e2e test working injecting data for 90 days.
    
    Resolve: #28

--- plugin/src/test/java/org/opennms/timeseries/cortex/CortexTSSIntegrationTest.java
+++ plugin/src/test/java/org/opennms/timeseries/cortex/CortexTSSIntegrationTest.java
@@ -39,8 +39,7 @@ public class CortexTSSIntegrationTest extends AbstractStorageIntegrationTest {
 
     @ClassRule
     public static DockerComposeContainer<?> environment = new DockerComposeContainer<>(new File("src/test/resources/org/opennms/timeseries/cortex/docker-compose.yaml"))
-                .withExposedService("cortex", 9009, Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(10)))
-                .withExposedService("grafana", 3000, Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(10)));
+                .withExposedService("cortex", 9009, Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(10)));
 
     private CortexTSS cortexTss;
 
@@ -105,7 +104,7 @@ public class CortexTSSIntegrationTest extends AbstractStorageIntegrationTest {
             time = time.plus(1, ChronoUnit.HOURS);
         }
         storage.store(originalSamples);
-        // The writes are async. Therefore we need to wait a bit before reading...
+        // The writes are async. Therefore, we need to wait a bit before reading...
         Thread.sleep(10000);
 
         TimeSeriesFetchRequest request = ImmutableTimeSeriesFetchRequest.builder()
